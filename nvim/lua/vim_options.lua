6a7b8e7ff736940f940b602d257cea7f4d2a2f21
Ryan Rolon <ryan.rolon@upr.edu>
2025-08-03 22:10:58 -0400

    First commit

---
+++ nvim/lua/vim_options.lua
@@ -0,0 +1,52 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+vim.opt.mouse="a"
+vim.opt.encoding = "utf-8"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.cmd [[ set noswapfile ]]
+vim.cmd [[ set termguicolors ]]
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>nh', ':nohlsearch<CR>')
+
+-- Tab keymaps
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>')
+vim.keymap.set('n', '<leader>tc', ':tabclose<CR>')
+vim.keymap.set('n', '<leader>tl', ':tabn<CR>')
+vim.keymap.set('n', '<leader>th', ':tabp<CR>')
+
+-- Quality of life mappings
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'JJ', '<Esc>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>d", ":Alpha<CR>", { noremap = true, silent = true })
+
+-- o and O without entering insert mode
+vim.keymap.set("n", "o", "o<Esc>", { noremap = true })
+vim.keymap.set("n", "O", "O<Esc>", { noremap = true })
+
+--Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
---
+++ spotify/Users/31kyia3nbmuk4rbt7ii43lfyofbu-user/frecency.pb
@@ -0,0 +1,23 @@
+
+>
+'spotify:playlist:37i9dQZF1EQoqCH7BwIYb7	ma¾+ày@ ÿëíÁ
+>
+'spotify:playlist:37i9dQZF1EIVOlGHvpBwBH	ÅÓÒoáy@ Ô§‚Â
+>
+'spotify:playlist:37i9dQZF1DX5DYkVgiO6RF	"È/Qäày@ þ¿ùÁ
+>
+'spotify:playlist:37i9dQZF1EIUVhDkMc6n10	”Ï“¡!äy@ Ûï­Â
+`
+4spotify:user:31kyia3nbmuk4rbt7ii43lfyofbu:collection	|­m²y@ ëâë¾	™§´ZíÞy@ §ÞÙÁ
+>
+'spotify:playlist:37i9dQZF1EIYf6cItAYzfX	¹* _£åy@ êÆÂ
+>
+'spotify:playlist:37i9dQZF1EQpj7X7UK8OOF	4zÍöåy@ Þ¾ËÂ
+>
+'spotify:playlist:37i9dQZF1E382MH1poUGtW	¤\·‡íy@ ™éÅÃ
+>
+'spotify:playlist:37i9dQZF1EQnsJ0xmvpihE	zðÈØØïy@ à£ëÃ
+>
+'spotify:playlist:37i9dQZF1EYkqdzj48dyYq	Æ×Ti´ôy@ ‡è¹Ä
+>
+'spotify:playlist:37i9dQZF1EVHGWrwldPRtj	¢¿¬“šúy@ •¦»Ä
